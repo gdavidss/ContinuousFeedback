3c2b6316965068f509577fc881924487928b3ba7
gdavidss <davidguilherme.david@gmail.com>
2022-12-03 19:38:23 -0300

    Initial Commit

---
+++ continuousfeedback/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  continuousfeedback
+//
+//  Created by Guilherme David on 03/12/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ continuousfeedback/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  continuousfeedback
+//
+//  Created by Guilherme David on 03/12/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

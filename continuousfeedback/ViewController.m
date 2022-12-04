f941c33d30908705da5c3ff679871fbc3286a019
gdavidss <davidguilherme.david@gmail.com>
2022-12-04 13:21:24 -0300

    added ability to tap label to turn it upside down

--- continuousfeedback/ViewController.m
+++ continuousfeedback/ViewController.m
@@ -6,6 +6,7 @@
 @property(nonatomic) CGFloat red;
 @property(nonatomic) CGFloat green;
 @property(nonatomic) UILabel *label;
+@property(nonatomic) CGFloat tickMultiplier;
 @property(nonatomic) UIPanGestureRecognizer *panGesture;
 @property(nonatomic, strong) UIScrollView *scrollView;
 @property(nonatomic, strong) AVAudioPlayer *player;
@@ -16,7 +17,9 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-
+    
+  _tickMultiplier = 2.5;
+    
   [self setupAudioPlayer];
   [self setupView];
   [self setupGestureRecognizer];
@@ -35,57 +38,81 @@
   [session setCategory:AVAudioSessionCategoryPlayback error:nil];
 
   // Create the audio player
-  self.player = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:nil];
-
-  self.player.numberOfLoops =
+  _player = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:nil];
+    
+  _player.numberOfLoops =
       -1;  // -1 means the sound will be played indefinitely
 
-  self.player.rate = 1;
+  _player.rate = 1;
   // Check if the sound file supports variable playback speed
-  self.player.enableRate = YES;
+  _player.enableRate = YES;
 
-  [self.player play];
+  [_player play];
 }
 
 - (void)setupView {
   // Initialize the colors
-  self.red = 0.5;
-  self.green = 0.5;
+  _red = 0.5;
+  _green = 0.5;
 
   // Set the background color of the view to the initial color
-  self.view.backgroundColor = [UIColor colorWithRed:self.red
-                                              green:self.green
+  self.view.backgroundColor = [UIColor colorWithRed:_red
+                                              green:_green
                                                blue:0.0
                                               alpha:1.0];
 
   // Create the scroll view and set its delegate to self
-  self.scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
-  self.scrollView.delegate = self;
-  [self.view addSubview:self.scrollView];
+  _scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+  _scrollView.delegate = self;
+  [self.view addSubview:_scrollView];
 }
 
 - (void)setupGestureRecognizer {
   // Initialize the pan gesture
-  self.panGesture =
+  _panGesture =
       [[UIPanGestureRecognizer alloc] initWithTarget:self
                                               action:@selector(handlePan:)];
-  self.panGesture.delegate = self;
-  [self.scrollView addGestureRecognizer:self.panGesture];
+  _panGesture.delegate = self;
+  [_scrollView addGestureRecognizer:_panGesture];
 }
 
+
+- (void)labelTapped:(UITapGestureRecognizer *)sender {
+  // Check if the label is upside down
+  if (CGAffineTransformEqualToTransform(self.label.transform, CGAffineTransformMakeRotation(M_PI))) {
+    // If it is, rotate it back to its normal orientation
+    self.label.transform = CGAffineTransformMakeRotation(0);
+      _label.frame = CGRectMake(0, 0, self.view.frame.size.width * 0.4,
+                                self.view.frame.size.height * 0.3);
+  } else {
+    // If it's not, rotate it upside down
+    self.label.transform = CGAffineTransformMakeRotation(M_PI);
+    _label.frame = CGRectMake(0, 0, self.view.frame.size.width * 1.6,
+                                self.view.frame.size.height * 0.3);
+  }
+}
+
+
 - (void)setupLabel {
-  self.label = [[UILabel alloc] init];
-  self.label.font = [self.label.font fontWithSize:80];
+  _label = [[UILabel alloc] init];
+  _label.font = [_label.font fontWithSize:80];
+  _label.font = [UIFont fontWithName:@"Avenir" size:80];
 
   // Set the text of the label to the percentage value
   _label.text = @"50";
 
   // Set the text color of the label to white
   _label.textColor = [UIColor whiteColor];
+    
+    // Add a tap gesture recognizer to the label
+    UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(labelTapped:)];
+    [self.label addGestureRecognizer:tapRecognizer];
+    self.label.userInteractionEnabled = YES;
+
 
   // Create an NSMutableAttributedString object with the text of the label
   NSMutableAttributedString *attributedString =
-      [[NSMutableAttributedString alloc] initWithString:self.label.text];
+      [[NSMutableAttributedString alloc] initWithString:_label.text];
 
   // Set the stroke color of the attributed string to black
   [attributedString addAttribute:NSStrokeColorAttributeName
@@ -98,7 +125,7 @@
                            range:NSMakeRange(0, attributedString.length)];
 
   // Set the attributedText property of the label to the attributed string
-  self.label.attributedText = attributedString;
+  _label.attributedText = attributedString;
 
   // Center the text in the label
   _label.textAlignment = NSTextAlignmentCenter;
@@ -117,21 +144,20 @@
   CGFloat y = scrollView.contentOffset.y;
 
   // Update the colors based on the scroll position
-  self.green =
-      MAX(0.0, MIN(1.0, 0.5 - y / self.scrollView.frame.size.height * 2));
-  self.red =
-      MAX(0.0, MIN(1.0, 0.5 + y / self.scrollView.frame.size.height * 2));
+  _green =
+      MAX(0.0, MIN(1.0, 0.5 - y / _scrollView.frame.size.height * 2));
+  _red =
+      MAX(0.0, MIN(1.0, 0.5 + y / _scrollView.frame.size.height * 2));
 
   NSString *stringValue =
-      [NSString stringWithFormat:@"%.2d", (int)floor(self.green * 100)];
+      [NSString stringWithFormat:@"%.2d", (int)floor(_green * 100)];
   _label.text = stringValue;
 
-  NSLog(@"%f", self.green);
-  self.player.rate = self.green * 2.5;
+  _player.rate = _green * _tickMultiplier;
 
   // Update the background color of the view
-  self.view.backgroundColor = [UIColor colorWithRed:self.red
-                                              green:self.green
+  self.view.backgroundColor = [UIColor colorWithRed:_red
+                                              green:_green
                                                blue:0.0
                                               alpha:1.0];
 }
@@ -140,12 +166,12 @@
 
 - (void)handlePan:(UIPanGestureRecognizer *)gesture {
   // Update the scroll view's content offset based on the user's pan gesture
-  CGPoint translation = [gesture translationInView:self.scrollView];
+  CGPoint translation = [gesture translationInView:_scrollView];
   CGPoint newOffset =
-      CGPointMake(self.scrollView.contentOffset.x,
-                  self.scrollView.contentOffset.y + translation.y);
-  self.scrollView.contentOffset = newOffset;
-  [gesture setTranslation:CGPointZero inView:self.scrollView];
+      CGPointMake(_scrollView.contentOffset.x,
+                  _scrollView.contentOffset.y + translation.y);
+  _scrollView.contentOffset = newOffset;
+  [gesture setTranslation:CGPointZero inView:_scrollView];
 }
 
 @end
